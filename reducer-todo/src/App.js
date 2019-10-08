795f1900af2e2090aba83c8a7a1856eea13c9f52
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-08 17:25:45 +0100

    commit code from end of day -more work to be done

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-
+import React, { useReducer } from "react";
+import Todos from "./components/Todos";
+import TodoForm from "./components/TodoForm";
+import reducer, {
+  initialState,
+  ADD_TODO,
+  ON_CHANGE,
+  MARK_COMPLETE,
+  CLEAR_COMPLETE
+} from "./reducers/reducer";
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      
+      <h1>React Todo List</h1>
+      <Todos todos={todos} dispatch={dispatch} />
+      <TodoForm ADD_TODO={ADD_TODO} dispatch={dispatch}/>
     </div>
   );
 }
--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,20 +1,45 @@
 import React, { useReducer } from "react";
 
-export const initialState = 
+export const initialState = {
+  todos: [
     {
-        item: 'Learn about reducers',
-        completed: false,
-        id: 3892987589
-      }
-  
+      item: "Learn about reducers",
+      completed: false,
+      id: new Date()
+    },
 
-export function reducer(state, action){
-    switch(action.type){
-        case
-
-    default: 
-    return state;
+    {
+      item: "Learn Redux",
+      completed: false,
+      id: new Date()
     }
+  ],
+  newTodo: "",
 };
 
+export const ADD_TODO = "ADD_TODO";
+export const ON_CHANGE = "ON_CHANGE";
+export const MARK_COMPLETE = "MARK_COMPLETE";
+export const CLEAR_COMPLETE = "CLEAR_COMPLETE";
 
+export default function reducer(state, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      debugger;
+      return {
+        ...state,
+        todos: 
+        state.todos.concat ( {
+            id: Date.now(),
+            item: state.item,
+            completed: false
+          }) 
+      };
+    case MARK_COMPLETE:
+      return state;
+    case CLEAR_COMPLETE:
+      return state;
+    default:
+      return state;
+  }
+}
