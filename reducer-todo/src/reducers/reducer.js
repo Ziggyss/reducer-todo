47372017b29c0f0478cdbfb83244373614b0df7b
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-08 12:53:14 +0100

    set up project and begin writing reducer

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,20 @@
+import React, { useReducer } from "react";
+
+export const initialState = 
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+      }
+  
+
+export function reducer(state, action){
+    switch(action.type){
+        case
+
+    default: 
+    return state;
+    }
+};
+
+
